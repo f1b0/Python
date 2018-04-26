9b9fe6078ec589bb7b1442afd4792156f929e46b
f1b0 <f1b0@f1b0s-MacBook-Pro.local>
2018-04-26 22:42:48 +0200

    update v2 xmlReader

--- xml/xmlReader.py
+++ xml/xmlReader.py
@@ -26,14 +26,14 @@ def searchInXml(xml, search_pattern='INC0', foundList=[]):
             else:
                 print(item)
 
-def xmlReader(xml_folder):
-    for files in os.listdir(xml_folder):
-        f = xml_folder + files
-        with open(f) as fd:
-            xml_dict = xml2Dict(fd)
-            search = xmlIterator(xml_dict)
-            searchInXml(search)
-            input('######### ENTER #########')
+# def xmlReader(xml_folder):
+#     for files in os.listdir(xml_folder):
+#         f = xml_folder + files
+#         with open(f) as fd:
+#             xml_dict = xml2Dict(fd)
+            #search = xmlIterator(xml_dict)
+            #searchInXml(search)
+            #input('######### ENTER #########')
 
 def main():
     xml_folder = '/Users/f1b0/Workspace/AssuranceTriangle/xml_folder'
