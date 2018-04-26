cbb413aa3c13ae292e4b6adcc2e2554afc56a789
f1b0 <f1b0@f1b0s-MacBook-Pro.local>
2018-04-26 23:45:53 +0200

    updated v3 xmlReader

--- xml/xmlReader.py
+++ xml/xmlReader.py
@@ -14,29 +14,29 @@ def xmlIterator(item, foundList=[]):
     for key in item.keys():
         if type(item[key]) == dict:
             xmlIterator(item[key])
+        elif type(item[key]) == list:
+            for x in range(0, len(item[key])):
+                xmlIterator(item[key][x])
         else:
             foundList.append(item[key])
     return foundList
 
-def searchInXml(xml, search_pattern='INC0', foundList=[]):
+def searchInXml(xml, search_pattern, foundList=[]):
     for item in xml:
         if search_pattern in str(item):
-            if type(item) == list:
-                searchInXml(item[0])
-            else:
-                print(item)
+            return item
 
-# def xmlReader(xml_folder):
-#     for files in os.listdir(xml_folder):
-#         f = xml_folder + files
-#         with open(f) as fd:
-#             xml_dict = xml2Dict(fd)
-            #search = xmlIterator(xml_dict)
-            #searchInXml(search)
-            #input('######### ENTER #########')
+def xmlReader(xml_folder):
+    for files in os.listdir(xml_folder):            # iterate through all xml files in given folder
+        xml_file = xml_folder + files               # file path + file name
+        with open(xml_file) as xml:                 # open xml file as xml
+            xml_dict = xml2Dict(xml)                # convert xml <-> dictionary
+            search = xmlIterator(xml_dict)
+            print(searchInXml(search, 'SAP@'))
+            input('######### ENTER #########')
 
 def main():
-    xml_folder = '/Users/f1b0/Workspace/AssuranceTriangle/xml_folder'
+    xml_folder = '/Users/f1b0/Workspace/AssuranceTriangle/xml_folder/'
     xmlReader(xml_folder)
 
 if __name__ == '__main__':
